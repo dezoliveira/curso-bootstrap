32fe906e29fb8dd705878bb2b56f8ac77822e777
Andres Oliveira <andres.oliveira@zeeng.com.br>
2021-10-12 15:08:38 -0300

    Offcanvas, Pagination, Popover, Progress Bar, Scrollspy

---
+++ Components/Popover/js/script.js
@@ -0,0 +1,8 @@
+let popoverTriggerList = [].slice.call(
+    document.querySelectorAll(['[data-bs-toggle="popover"]'])
+)
+
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl){
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+
